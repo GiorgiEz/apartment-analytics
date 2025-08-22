c230cf5d116437410eaed0c8a8d2a57ab1f9104c
giorgi <liparteliani.giorgi@kiu.edu.ge>
2025-08-22 20:20:18 +0400

    frontend done for now

--- frontend/src/js/helpers/icons.js
+++ frontend/src/js/helpers/icons.js
@@ -52,5 +52,17 @@ export const icons = {
                 d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 
                 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
         </svg>
+    `,
+    error_svg: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto text-red-500 mb-4" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 
+                0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+    `,
+    image_placeholder: `
+       <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mx-auto mb-2" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+       </svg>
     `
 }
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -38,13 +38,11 @@ class App {
             console.error('Failed to load view:', error);
             this.mainContent.innerHTML = `
                 <div class="flex items-center justify-center h-full">
-                    <div class="text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 mx-auto text-red-500 mb-4" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                        </svg>
-                        <h2 class="text-xl font-semibold text-steel-800 mb-2">Failed to load content</h2>
-                        <p class="text-steel-600">Please try again later.</p>
-                        <button onclick="location.reload()" class="mt-4 px-4 py-2 bg-steel-600 text-white rounded-lg hover:bg-steel-500">Retry</button>
+                    <div class="text-center">${icons.error_svg}
+                        <h2 class="text-xl font-semibold text-blue-800 mb-2">Failed to load content</h2>
+                        <p class="text-blue-600">Please try again later.</p>
+                        <button onclick="location.reload()" class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg 
+                            hover:bg-blue-500">Retry</button>
                     </div>
                 </div>
             `;
