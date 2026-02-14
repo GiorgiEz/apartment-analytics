e76e9d5efcd0768ba80eb5d8197439c6605faeae
giorgi <liparteliani.giorgi@kiu.edu.ge>
2026-02-14 19:56:24 +0400

    added responsive dashboard, loading visualization charts

--- frontend/src/js/analysis.js
+++ frontend/src/js/analysis.js
@@ -1,6 +1,44 @@
-window.initAnalysisView = function(){
-    // Wait for DOM to be fully ready
-    setTimeout(() => {
-        console.log("To Be Implemented")
-    }, 100); // Small delay to ensure DOM is ready
+import { initModal, renderAnalysisSection } from "@components/analysis_modal.js";
+import {ANALYSIS_SECTIONS,
+        marketOverviewCharts, locationInsightsCharts, apartmentCharacteristicsCharts,
+        priceAnalysisCharts, timeAnalysisCharts}
+    from "@helpers/helper_functions"
+
+
+window.initAnalysisView = function () {
+    initModal();
+
+    setTimeout(async () => {
+        const analysis_sections_len = ANALYSIS_SECTIONS.length
+        let currentIndex = 0;
+
+        const prevBtn = document.getElementById("analysis-prev");
+        const nextBtn = document.getElementById("analysis-next");
+        const title = document.getElementById("analysis-section-title");
+
+        const renderMap = {
+            0: marketOverviewCharts,
+            1: priceAnalysisCharts,
+            2: apartmentCharacteristicsCharts,
+            3: locationInsightsCharts,
+            4: timeAnalysisCharts,
+        }
+
+        function renderSection() {
+            title.textContent = ANALYSIS_SECTIONS[currentIndex];
+            renderAnalysisSection(renderMap[currentIndex]);
+        }
+
+        prevBtn.addEventListener("click", () => {
+            currentIndex = (currentIndex - 1 + analysis_sections_len) % analysis_sections_len
+            renderSection();
+        });
+
+        nextBtn.addEventListener("click", () => {
+            currentIndex = (currentIndex + 1) % analysis_sections_len;
+            renderSection();
+        });
+
+        renderSection();
+    }, 100);
 };
