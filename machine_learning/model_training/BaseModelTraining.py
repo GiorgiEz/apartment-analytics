e7b54d6862441ab0d641f346b416054c8c3ac6fe
giorgi <lipartelianige@gmail.com>
2026-04-09 17:01:49 +0400

    small model hyper parameter modifications, added cross val score testing before fitting

--- machine_learning/model_training/BaseModelTraining.py
+++ machine_learning/model_training/BaseModelTraining.py
@@ -5,6 +5,7 @@ from sklearn.compose import ColumnTransformer
 import numpy as np
 import pandas as pd
 import os, joblib
+from sklearn.model_selection import cross_val_score
 
 
 
@@ -30,6 +31,7 @@ class BaseModelTraining(ABC):
 
         self.categorical_features = [
             "city",
+            # "district_name"
             "floor_bucket",
             "district_grouped",
             "area_bucket",
@@ -71,6 +73,10 @@ class BaseModelTraining(ABC):
         # After tuning is done we can combine validation df with training df and use them for training
         X_full = pd.concat([self.X_train, self.X_val])
         y_full = pd.concat([self.y_train, self.y_val])
+
+        # ---- Cross-validation (UNFITTED pipeline) ----
+        cv_scores = cross_val_score(self.pipeline, X_full, y_full, cv=5, scoring="r2", n_jobs=-1)
+        print(f"Model: {self.name}. CV R2 mean: {cv_scores.mean():.4f}, CV R2 std: {cv_scores.std():.4f}")
 
         self.pipeline.fit(X_full, y_full)
 
